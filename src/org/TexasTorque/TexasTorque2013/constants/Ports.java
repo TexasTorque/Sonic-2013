e62cdfd67032c29ac4d8799ea66b2bd3c65f4bfc
tommista <cprocket.sos@gmail.com>
2013-01-09 11:57:47 -0600

    rearranged ports to look prettyer

--- src/org/TexasTorque/TexasTorque2013/constants/Ports.java
+++ src/org/TexasTorque/TexasTorque2013/constants/Ports.java
@@ -11,14 +11,16 @@ public class Ports
     public static int OPERATOR_CONTROLLER_PORT = 2;
     
     //----- Motors -----
-    public static int FRONT_LEFT_MOTOR_PORT = 1;
-    public static int MIDDLE_LEFT_MOTOR_PORT = 2;
-    public static int REAR_LEFT_MOTOR_PORT = 3;
-    public static int FRONT_RIGHT_MOTOR_PORT = 4;
-    public static int MIDDLE_RIGHT_MOTOR_PORT = 5;
-    public static int REAR_RIGHT_MOTOR_PORT = 6;
-    public static int FRONT_SHOOTER_MOTOR_PORT = 1;
-    public static int REAR_SHOOTER_MOTOR_PORT = 2;
+        //----- Sidecar 1 -----
+        public static int FRONT_LEFT_MOTOR_PORT = 1;
+        public static int MIDDLE_LEFT_MOTOR_PORT = 2;
+        public static int REAR_LEFT_MOTOR_PORT = 3;
+        public static int FRONT_RIGHT_MOTOR_PORT = 4;
+        public static int MIDDLE_RIGHT_MOTOR_PORT = 5;
+        public static int REAR_RIGHT_MOTOR_PORT = 6;
+        //----- Sidecar 2 -----
+        public static int FRONT_SHOOTER_MOTOR_PORT = 1;
+        public static int REAR_SHOOTER_MOTOR_PORT = 2;
     
     //----- Solenoids -----
     public static int SHIFTERS_FORWARD_PORT = 1;
