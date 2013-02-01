a8b16b46a8aca891a11c93fc37c948e58e72610c
tommista <cprocket.sos@gmail.com>
2013-02-01 16:58:19 -0600

    changed port order

--- src/org/TexasTorque/TexasTorque2013/constants/Ports.java
+++ src/org/TexasTorque/TexasTorque2013/constants/Ports.java
@@ -29,9 +29,9 @@ public class Ports
     
     //----- Solenoids -----
     public static int DRIVE_SHIFTER_PORT = 1;
-    public static int LOADER_SOLENOID_PORT = 2;
-    public static int FRISBEE_LIFTER_SOLENOID_A_PORT = 3;
-    public static int FRISBEE_LIFTER_SOLENOID_B_PORT = 4;
+    public static int FRISBEE_LIFTER_SOLENOID_A_PORT = 2;
+    public static int FRISBEE_LIFTER_SOLENOID_B_PORT = 3;
+    public static int LOADER_SOLENOID_PORT = 4;
     
     //----- Digital Inputs -----
         //----- Sidecar 1 -----
