8c4b91f55b26728b439d04d436bda55128a323c5
Bradley Holloway <drummerboy400@gmail.com>
2013-02-07 16:02:59 -0600

    started working on frontpyrammid autonomous

--- src/org/TexasTorque/TexasTorque2013/autonomous/AutonomousBase.java
+++ src/org/TexasTorque/TexasTorque2013/autonomous/AutonomousBase.java
@@ -4,7 +4,14 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Watchdog;
 import org.TexasTorque.TexasTorque2013.io.RobotOutput;
 import org.TexasTorque.TexasTorque2013.io.SensorInput;
+import org.TexasTorque.TexasTorque2013.subsystem.drivebase.Drivebase;
+import org.TexasTorque.TexasTorque2013.subsystem.manipulator.Elevator;
+import org.TexasTorque.TexasTorque2013.subsystem.manipulator.Intake;
+import org.TexasTorque.TexasTorque2013.subsystem.manipulator.Magazine;
+import org.TexasTorque.TexasTorque2013.subsystem.manipulator.Manipulator;
+import org.TexasTorque.TexasTorque2013.subsystem.manipulator.Shooter;
 import org.TexasTorque.TorqueLib.util.DashboardManager;
+import org.TexasTorque.TorqueLib.util.Parameters;
 
 public abstract class AutonomousBase
 {
@@ -13,7 +20,13 @@ public abstract class AutonomousBase
     protected Watchdog watchdog;
     protected RobotOutput robotOutput;
     protected SensorInput sensorInput;
+    protected Drivebase drivebase;
+    protected Intake intake;
+    protected Shooter shooter;
+    protected Elevator elevator;
+    protected Magazine magazine;
     protected DashboardManager dashboardManager;
+    protected Parameters params;
     
     public AutonomousBase()
     {
@@ -22,6 +35,12 @@ public abstract class AutonomousBase
         robotOutput = RobotOutput.getInstance();
         sensorInput = SensorInput.getInstance();
         dashboardManager = DashboardManager.getInstance();
+        drivebase = Drivebase.getInstance();
+        intake = Intake.getInstance();
+        shooter = Shooter.getInstance();
+        elevator = Elevator.getInstance();
+        magazine = Magazine.getInstance();
+        params = Parameters.getInstance();
     }
     
     public abstract void init();
