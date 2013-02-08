d3b59058b431a5b430fca3112cbab96435bb33d3
Bradley Holloway <drummerboy400@gmail.com>
2013-02-07 19:13:27 -0600

    Changed all ports and constants to final...

--- src/org/TexasTorque/TexasTorque2013/constants/Constants.java
+++ src/org/TexasTorque/TexasTorque2013/constants/Constants.java
@@ -3,41 +3,41 @@ package org.TexasTorque.TexasTorque2013.constants;
 public class Constants
 {
     //----- Deadbands -----
-    public static double SPEED_AXIS_DEADBAND = 0.07;
-    public static double TURN_AXIS_DEADBAND = 0.07;
+    public final static double SPEED_AXIS_DEADBAND = 0.07;
+    public final static double TURN_AXIS_DEADBAND = 0.07;
     
     //----- Autonomous -----
-    public static int DO_NOTHING_AUTO = 0;
-    public static int DIAGNOSTIC_AUTO = 1;
-    public static int FRONT_SHOOT_AUTO = 2;
-    public static int FRONT_DRIVE_AUTO = 3;
-    public static int REAR_SHOOT_AUTO = 4;
-    public static int REAR_DRIVE_FORWARD_AUTO = 5;
-    public static int REAR_DRIVE_FORWARD_DOUBLE_AUTO = 6;
-    public static int REAR_DRIVE_REVERSE_AUTO = 7;
-    public static int REAR_DRIVE_REVERSE_SHOOT_AUTO = 8;
+    public final static int DO_NOTHING_AUTO = 0;
+    public final static int DIAGNOSTIC_AUTO = 1;
+    public final static int FRONT_SHOOT_AUTO = 2;
+    public final static int FRONT_DRIVE_AUTO = 3;
+    public final static int REAR_SHOOT_AUTO = 4;
+    public final static int REAR_DRIVE_FORWARD_AUTO = 5;
+    public final static int REAR_DRIVE_FORWARD_DOUBLE_AUTO = 6;
+    public final static int REAR_DRIVE_REVERSE_AUTO = 7;
+    public final static int REAR_DRIVE_REVERSE_SHOOT_AUTO = 8;
     
     //----- Drivebase -----
-    public static double MAX_DIAMETER = 1.5;
-    public static double ROBOT_WIDTH = 1.0;
-    public static double DEFAULT_LOW_SENSITIVITY = 1.25;
-    public static double DEFAULT_HIGH_SENSITIVITY = 0.7;
+    public final static double MAX_DIAMETER = 1.5;
+    public final static double ROBOT_WIDTH = 1.0;
+    public final static double DEFAULT_LOW_SENSITIVITY = 1.25;
+    public final static double DEFAULT_HIGH_SENSITIVITY = 0.7;
     
     //----- Gyro -----
-    public static int GYRO_ACCUMULATOR_DEADBAND = 75;
-    public static double GYRO_SENSITIVITY = 0.0136;
+    public final static int GYRO_ACCUMULATOR_DEADBAND = 75;
+    public final static double GYRO_SENSITIVITY = 0.0136;
     
     //----- Shooter -----
-    public static int TILT_PARALLEL_POSITION = 5;
-    public static double POTENTIOMETER_LOW_VOLTAGE = 2.5;
-    public static double POTENTIOMETER_HIGH_VOLTAGE = 5.0;
-    public static int DEFAULT_FRONT_SHOOTER_RATE = 3000;
-    public static int DEFAULT_REAR_SHOOTER_RATE = 2000;
-    public static int SHOOTER_STOPPED_RATE = 0;
+    public final static int TILT_PARALLEL_POSITION = 5;
+    public final static double POTENTIOMETER_LOW_VOLTAGE = 2.5;
+    public final static double POTENTIOMETER_HIGH_VOLTAGE = 5.0;
+    public final static int DEFAULT_FRONT_SHOOTER_RATE = 3000;
+    public final static int DEFAULT_REAR_SHOOTER_RATE = 2000;
+    public final static int SHOOTER_STOPPED_RATE = 0;
     
     //----- Elevator -----
-    public static int DEFAULT_ELEVATOR_BOTTOM_POSITION = 0;
-    public static int DEFAULT_ELEVATOR_TOP_POSITION = 1;
+    public final static int DEFAULT_ELEVATOR_BOTTOM_POSITION = 0;
+    public final static int DEFAULT_ELEVATOR_TOP_POSITION = 1;
     
     //----- Magazine -----
     public final static int MAGAZINE_READY_STATE = 0;
@@ -46,7 +46,7 @@ public class Constants
     public final static int MAGAZINE_RESETTING_STATE = 3;
     
     //----- Misc -----
-    public static int TORQUE_LOGGING_LOOP_TIME = 20;
-    public static double MOTOR_STOPPED = 0.0;
+    public final static int TORQUE_LOGGING_LOOP_TIME = 20;
+    public final static double MOTOR_STOPPED = 0.0;
     
 }
--- src/org/TexasTorque/TexasTorque2013/constants/Ports.java
+++ src/org/TexasTorque/TexasTorque2013/constants/Ports.java
@@ -3,62 +3,62 @@ package org.TexasTorque.TexasTorque2013.constants;
 public class Ports
 {
     //----- Misc -----
-    public static int SIDECAR_ONE = 1;
-    public static int SIDECAR_TWO = 2;
+    public final static int SIDECAR_ONE = 1;
+    public final static int SIDECAR_TWO = 2;
     
     //----- Controllers -----
-    public static int DRIVE_CONTROLLER_PORT = 1;
-    public static int OPERATOR_CONTROLLER_PORT = 2;
+    public final static int DRIVE_CONTROLLER_PORT = 1;
+    public final static int OPERATOR_CONTROLLER_PORT = 2;
     
     //----- Motors -----
         //----- Sidecar 1 -----
 
-        public static int FRONT_RIGHT_MOTOR_PORT = 2;
-        public static int REAR_RIGHT_MOTOR_PORT = 3;
-        public static int ELEVATOR_MOTOR_PORT_LEFT = 4;
-        public static int INTAKE_MOTOR_PORT = 5;
+        public final static int FRONT_RIGHT_MOTOR_PORT = 2;
+        public final static int REAR_RIGHT_MOTOR_PORT = 3;
+        public final static int ELEVATOR_MOTOR_PORT_LEFT = 4;
+        public final static int INTAKE_MOTOR_PORT = 5;
         
         //----- Sidecar 2 -----
-        public static int REAR_SHOOTER_MOTOR_A_PORT = 1;
-        public static int REAR_SHOOTER_MOTOR_B_PORT = 2;
-        public static int FRONT_SHOOTER_MOTOR_A_PORT = 3;
-        public static int FRONT_SHOOTER_MOTOR_B_PORT = 4;
-        public static int FRONT_LEFT_MOTOR_PORT = 5;
-        public static int REAR_LEFT_MOTOR_PORT = 6;
-        public static int ELEVATOR_MOTOR_PORT_RIGHT = 7;
-        public static int SHOOTER_TILT_MOTOR_PORT = 8;
+        public final static int REAR_SHOOTER_MOTOR_A_PORT = 1;
+        public final static int REAR_SHOOTER_MOTOR_B_PORT = 2;
+        public final static int FRONT_SHOOTER_MOTOR_A_PORT = 3;
+        public final static int FRONT_SHOOTER_MOTOR_B_PORT = 4;
+        public final static int FRONT_LEFT_MOTOR_PORT = 5;
+        public final static int REAR_LEFT_MOTOR_PORT = 6;
+        public final static int ELEVATOR_MOTOR_PORT_RIGHT = 7;
+        public final static int SHOOTER_TILT_MOTOR_PORT = 8;
     
     //----- Solenoids -----
-    public static int DRIVE_SHIFTER_PORT = 1;
-    public static int PTO_SOLENOID_A_PORT = 2;
-    public static int PTO_SOLENOID_B_PORT = 3;
-    public static int LOADER_SOLENOID_PORT = 4;
-    public static int FRISBEE_LIFTER_SOLENOID_A_PORT = 5;
-    public static int FRISBEE_LIFTER_SOLENOID_B_PORT = 6;
-    public static int WHEELY_BAR_SOLENOID_PORT = 7;
+    public final static int DRIVE_SHIFTER_PORT = 1;
+    public final static int PTO_SOLENOID_A_PORT = 2;
+    public final static int PTO_SOLENOID_B_PORT = 3;
+    public final static int LOADER_SOLENOID_PORT = 4;
+    public final static int FRISBEE_LIFTER_SOLENOID_A_PORT = 5;
+    public final static int FRISBEE_LIFTER_SOLENOID_B_PORT = 6;
+    public final static int WHEELY_BAR_SOLENOID_PORT = 7;
     
     //----- Digital Inputs -----
         //----- Sidecar 1 -----
-        public static int PRESSURE_SWITCH_PORT = 13;
-        public static int RIGHT_DRIVE_ENCODER_A_PORT = 1;
-        public static int RIGHT_DRIVE_ENCODER_B_PORT = 2;
-        public static int FRONT_SHOOTER_COUNTER_A_PORT = 6;
-        public static int FRONT_SHOOTER_COUNTER_B_PORT = 7;
-        public static int REAR_SHOOTER_COUNTER_A_PORT = 8;
-        public static int REAR_SHOOTER_COUNTER_B_PORT = 9;
-        public static int WHEELY_BAR_SWITCH_PORT = 10;
-        public static int ELEVATOR_ENCODER_A_PORT = 11;
-        public static int ELEVATOR_ENCODER_B_PORT = 12;
+        public final static int PRESSURE_SWITCH_PORT = 13;
+        public final static int RIGHT_DRIVE_ENCODER_A_PORT = 1;
+        public final static int RIGHT_DRIVE_ENCODER_B_PORT = 2;
+        public final static int FRONT_SHOOTER_COUNTER_A_PORT = 6;
+        public final static int FRONT_SHOOTER_COUNTER_B_PORT = 7;
+        public final static int REAR_SHOOTER_COUNTER_A_PORT = 8;
+        public final static int REAR_SHOOTER_COUNTER_B_PORT = 9;
+        public final static int WHEELY_BAR_SWITCH_PORT = 10;
+        public final static int ELEVATOR_ENCODER_A_PORT = 11;
+        public final static int ELEVATOR_ENCODER_B_PORT = 12;
         //----- Sidecar 2 -----
-        public static int LEFT_DRIVE_ENCODER_A_PORT = 1;
-        public static int LEFT_DRIVE_ENCODER_B_PORT = 2;
+        public final static int LEFT_DRIVE_ENCODER_A_PORT = 1;
+        public final static int LEFT_DRIVE_ENCODER_B_PORT = 2;
     
     //----- Analog Inputs -----
-    public static int GYRO_PORT = 1;
-    public static int PRESSURE_SENSOR_PORT = 2;
-    public static int TILT_POTENTIOMETER_PORT = 3;
+    public final static int GYRO_PORT = 1;
+    public final static int PRESSURE_SENSOR_PORT = 2;
+    public final static int TILT_POTENTIOMETER_PORT = 3;
     
     //----- Relays -----
-    public static int COMPRESSOR_RELAY_PORT = 1;
+    public final static int COMPRESSOR_RELAY_PORT = 1;
     
 }
