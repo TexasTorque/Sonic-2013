7b81032d102e5e92b959752a3ef021dc616e112b
Rayne Beridon <rberidon@gmail.com>
2013-02-12 17:45:55 -0600

    wrote AdaFruitLights class, added Vector that will hold Digital Inputs.

--- src/org/TexasTorque/TorqueLib/util/AdaFruitLights.java
+++ src/org/TexasTorque/TorqueLib/util/AdaFruitLights.java
@@ -1,6 +1,41 @@
 package org.TexasTorque.TorqueLib.util;
 
+import edu.wpi.first.wpilibj.DigitalOutput;
+import edu.wpi.first.wpilibj.Watchdog;
+import java.util.Vector;
+
 public class AdaFruitLights
 {
+    private Vector outputVector;
     
+    private int currentState;
+    
+    public AdaFruitLights(Vector outputs)
+    {
+        outputVector = outputs;
+    }
+    
+    public void setDesiredState (int state)
+    {
+        currentState = state;
+    }
+    
+    public void setState()
+    {
+        String byteString = Integer.toBinaryString(currentState);
+        for (int index = 0; index < outputVector.size(); index++)
+        {
+            Watchdog.getInstance().feed();
+            int tempIndex = byteString.length() - 1 - index;
+            char value = byteString.charAt(tempIndex);
+            if (value == 48)
+            {
+                ((DigitalOutput)(outputVector.elementAt(index))).set(false);
+            }
+            else if(value == 49)
+            {
+                ((DigitalOutput)(outputVector.elementAt(index))).set(true);
+            }
+        }
+    }
 }
