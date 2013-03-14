04dde42c356d14f0be877338f3bec6c0a4d28957
Bradley Holloway <drummerboy400@gmail.com>
2013-03-14 11:25:23 -0500

    Added pivotTurn and Turn to autonomous

---
+++ src/org/TexasTorque/TexasTorque2013/autonomous/drive/AutonomousPivotTurn.java
@@ -0,0 +1,59 @@
+package org.TexasTorque.TexasTorque2013.autonomous.drive;
+
+import edu.wpi.first.wpilibj.Timer;
+import org.TexasTorque.TexasTorque2013.autonomous.AutonomousCommand;
+
+public class AutonomousPivotTurn extends AutonomousCommand
+{
+    private Timer timeoutTimer;
+    private double timeoutSecs;
+    
+    private boolean firstCycle;
+    private boolean turnPositive;
+    private double goal;
+    
+    private double pivotTurnLeft;
+    private double pivotTurnRight;
+    
+    public AutonomousPivotTurn(double degrees, double leftSpeed, double rightSpeed, double timeout)
+    {
+        super();
+        goal = degrees;
+        this.firstCycle = true;
+        
+        pivotTurnLeft = leftSpeed;
+        pivotTurnRight = rightSpeed;
+        
+        timeoutSecs = timeout;
+        timeoutTimer = new Timer();
+    }
+
+    public void reset() {
+        System.err.println("reset");
+        
+        timeoutTimer.reset();
+        timeoutTimer.start();
+    }
+
+    public boolean run() {
+        if ( this.firstCycle){
+            this.firstCycle = false;
+            turnPositive = (goal>0) ? true: false;
+            goal += this.sensorInput.getGyroAngle();
+        }
+
+        drivebase.setDriveSpeeds(pivotTurnLeft, pivotTurnRight);
+        
+        if(timeoutTimer.get() > timeoutSecs)
+        {
+            return true;
+        }
+        
+        if (turnPositive && sensorInput.getGyroAngle()>=goal || !turnPositive && sensorInput.getGyroAngle()<=goal){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+}
